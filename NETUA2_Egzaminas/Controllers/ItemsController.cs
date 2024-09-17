93a82f619287de8a81dae80e8ffc854a2316e84b
senoliss <lameskauskas@gmail.com>
2024-09-17 22:26:33 +0300

    Creating Items controller

---
+++ NETUA2_Egzaminas/Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NETUA2_Egzaminas.API.DTOs;
+using NETUA2_Egzaminas.API.Interfaces;
+using NETUA2_Egzaminas.API.Services;
+using NETUA2_Egzaminas.DAL.Entities;
+using System.Net.Mime;
+
+namespace NETUA2_Egzaminas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+        private readonly IJwtService _jwtService;
+        private readonly ILogger<ItemsController> _logger;
+        private string loggingMessage;
+
+        public ItemsController(IItemService itemService, IJwtService jwtService, ILogger<ItemsController> logger)
+        {
+            _itemService = itemService;
+            _jwtService = jwtService;
+            _logger = logger;
+        }
+        [HttpGet("GetItemByID")]
+        public IActionResult GetItemById(int id)
+        {
+            loggingMessage = "";
+
+            loggingMessage = $"Trying to GET all users.";
+            _logger.LogInformation(loggingMessage);
+
+            return Ok(_itemService.GetItemById(id));
+        }
+    }
+}
---
+++ NETUA2_Egzaminas/Interfaces/IItemService.cs
@@ -0,0 +1,12 @@
+﻿using NETUA2_Egzaminas.API.DTOs;
+using NETUA2_Egzaminas.DAL.Entities;
+
+namespace NETUA2_Egzaminas.API.Interfaces
+{
+    public interface IItemService
+    {
+        Item GetItemById(int id);
+        Item GetItemByUserId(int userId); 
+        List<Item> GetAll();
+    }
+}
